24faee385cd5f80953f4012b6559ef472f78c10b
William Valerie <williamvalerie2@gmail.com>
2025-07-03 10:38:33 +0700

    Add fonts: Poppins, Montserrat, Nunito, and Inter

--- app/src/main/java/com/wesleyaldrich/pancook/ui/screens/PlannerScreen.kt
+++ app/src/main/java/com/wesleyaldrich/pancook/ui/screens/PlannerScreen.kt
@@ -6,10 +6,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import com.wesleyaldrich.pancook.ui.theme.inter
+import com.wesleyaldrich.pancook.ui.theme.montserrat
+import com.wesleyaldrich.pancook.ui.theme.nunito
+import com.wesleyaldrich.pancook.ui.theme.poppins
 
 @Composable
 fun PlannerScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Planner Screen")
+        Text("Planner Screen", fontFamily = inter, fontWeight = FontWeight.ExtraBold, fontSize = 36.sp)
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PlannerPagePreview () {
+    PlannerScreen()
 }
--- app/src/main/java/com/wesleyaldrich/pancook/ui/theme/Type.kt
+++ app/src/main/java/com/wesleyaldrich/pancook/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.wesleyaldrich.pancook.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.wesleyaldrich.pancook.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +34,40 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val poppins = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_thin, FontWeight.Thin),
+)
+
+val inter = FontFamily(
+    Font(R.font.inter_var, FontWeight.Light),
+    Font(R.font.inter_var, FontWeight.Normal),
+    Font(R.font.inter_var, FontWeight.Medium),
+    Font(R.font.inter_var, FontWeight.SemiBold),
+    Font(R.font.inter_var, FontWeight.Bold),
+    Font(R.font.inter_var, FontWeight.ExtraBold),
+)
+
+val nunito = FontFamily(
+    Font(R.font.nunito_light, FontWeight.Light),
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_semibold, FontWeight.SemiBold),
+    Font(R.font.nunito_bold, FontWeight.Bold),
+    Font(R.font.nunito_extrabold, FontWeight.ExtraBold),
+)
+val montserrat = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
 )
