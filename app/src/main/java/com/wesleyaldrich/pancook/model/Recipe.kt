3c271e9a619a44438ea140a95fc01a844e335eb5
William Valerie <williamvalerie2@gmail.com>
2025-07-13 14:07:11 +0700

    Refactor PlanCard and Recipe so it pass drawable image as Int not string from Url

--- app/src/main/java/com/wesleyaldrich/pancook/model/Recipe.kt
+++ app/src/main/java/com/wesleyaldrich/pancook/model/Recipe.kt
@@ -4,7 +4,7 @@ data class Recipe(
     val id: Int,
     val title: String,
     val description: String,
-    val imageUrl: String,
+    val image: Int,
     val ingredients: List<Ingredient>,
     val steps: List<String>,
     val servings: Int
